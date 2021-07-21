cdfb698422254de36e184cb5b06f99b36d1c0b05
Chloe Dawn <chloe@sapphic.dev>
2021-07-21 15:42:08 +0100

    Fix file reader leak, migrate to NIO and config path API

--- src/main/java/selic/re/discordbridge/DiscordBridgeMod.java
+++ src/main/java/selic/re/discordbridge/DiscordBridgeMod.java
@@ -2,22 +2,23 @@ package selic.re.discordbridge;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 
 import javax.security.auth.login.LoginException;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class DiscordBridgeMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-			var filename = "config/discord-bridge.json";
+			var configFile = FabricLoader.getInstance().getConfigDir().resolve("discord-bridge.json");
 			try {
-				DiscordBot.init(Config.reloadFromFile(filename), server);
+				DiscordBot.init(Config.reloadFromFile(configFile), server);
 			} catch (LoginException e) {
 				LogManager.getLogger().error("Can't initialize discord bridge: could not login: " + e.getMessage());
-			} catch (FileNotFoundException e) {
-				LogManager.getLogger().error("Can't initialize discord bridge: can't read config at " + filename + ": " + e.getMessage());
+			} catch (IOException e) {
+				LogManager.getLogger().error("Can't initialize discord bridge: can't read config at " + configFile + ": " + e.getMessage());
 			}
 		});
 	}
