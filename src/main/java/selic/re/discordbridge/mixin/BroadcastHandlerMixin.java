938f2cab80f0061b5dfaee664d81c1feec06ce7c
Chloe Dawn <chloe@sapphic.dev>
2022-03-27 14:07:37 +0100

    Update mixin injector to match mappings change

--- src/main/java/selic/re/discordbridge/mixin/BroadcastHandlerMixin.java
+++ src/main/java/selic/re/discordbridge/mixin/BroadcastHandlerMixin.java
@@ -14,9 +14,9 @@ import java.util.UUID;
 @Mixin(PlayerManager.class)
 abstract class BroadcastHandlerMixin {
     @Inject(
-        method = "broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V",
+        method = "broadcast(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V",
         at = @At("HEAD"), require = 1)
-    private void preBroadcastChatMessage(Text message, MessageType type, UUID sender, CallbackInfo info) {
+    private void preBroadcast(Text message, MessageType type, UUID sender, CallbackInfo info) {
         DiscordBot.instance().sendMessage(message);
     }
 }
