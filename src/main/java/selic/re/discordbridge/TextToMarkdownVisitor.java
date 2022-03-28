fe42974b3177a6334d015625667293ed0628e3ec
Chloe Dawn <chloe@sapphic.dev>
2022-03-28 11:30:09 +0100

    Refactor TextToMarkdownVisitor
     - Store last style instead of individual style flags
     - Use inline codeblocks for interactive components
     - Introduce constants for token character sequences
     - Flush builder on finish alongside style reset

--- src/main/java/selic/re/discordbridge/TextToMarkdownVisitor.java
+++ src/main/java/selic/re/discordbridge/TextToMarkdownVisitor.java
@@ -6,86 +6,96 @@ import net.minecraft.text.Style;
 import java.util.Optional;
 
 public class TextToMarkdownVisitor implements StringVisitable.StyledVisitor<Void> {
-    StringBuilder result = new StringBuilder();
-    boolean bold;
-    boolean italic;
-    boolean underline;
-    boolean strikethrough;
-    boolean spoiler;
+    private static final String BOLD_TOKEN = "**";
+    private static final String ITALIC_TOKEN = "*";
+    private static final String UNDERLINED_TOKEN = "__";
+    private static final String STRIKETHROUGH_TOKEN = "~~";
+    private static final String SPOILER_TOKEN = "||";
+    private static final String INLINE_CODE_TOKEN = "`";
+
+    private final StringBuilder markdown = new StringBuilder();
+    private Style lastStyle = Style.EMPTY;
 
     public String finish() {
-        if (spoiler) {
-            result.append("||");
-            spoiler = false;
-        }
-        if (strikethrough) {
-            result.append("~~");
-            strikethrough = false;
-        }
-        if (underline) {
-            result.append("__");
-            underline = false;
-        }
-        if (italic) {
-            result.append("*");
-            italic = false;
-        }
-        if (bold) {
-            result.append("**");
-            bold = false;
-        }
-        return result.toString();
+        this.popTokens(Style.EMPTY); // Pop dangling tokens
+
+        String markdown = this.markdown.toString();
+        this.markdown.setLength(0); // Flush builder
+        return markdown;
     }
 
     @Override
     public Optional<Void> accept(Style style, String text) {
-        // Applying new style
-        boolean shouldBeBold = style.isBold() || style.getHoverEvent() != null;
-        boolean shouldBeItalic = style.isItalic();
-        boolean shouldBeUnderlined = style.isUnderlined();
-        boolean shouldBeStrikethrough = style.isStrikethrough();
-        boolean shouldBeSpoiler = style.isObfuscated();
+        this.pushTokens(style);
+        this.popTokens(style);
 
-        if (shouldBeBold && !this.bold) {
-            result.append("**");
-        }
-        if (shouldBeItalic && !this.italic) {
-            result.append("*");
-        }
-        if (shouldBeUnderlined && !underline) {
-            result.append("__");
-        }
-        if (shouldBeStrikethrough && !this.strikethrough) {
-            result.append("~~");
-        }
-        if (shouldBeSpoiler && !spoiler) {
-            result.append("||");
-        }
-
-        // Removing existing style - REVERSE ORDER
-        if (!shouldBeSpoiler && spoiler) {
-            result.append("||");
-        }
-        if (!shouldBeStrikethrough && this.strikethrough) {
-            result.append("~~");
-        }
-        if (!shouldBeUnderlined && underline) {
-            result.append("__");
-        }
-        if (!shouldBeItalic && this.italic) {
-            result.append("*");
-        }
-        if (!shouldBeBold && this.bold) {
-            result.append("**");
-        }
-
-        result.append(text);
-        this.bold = shouldBeBold;
-        this.italic = shouldBeItalic;
-        this.strikethrough = shouldBeStrikethrough;
-        this.underline = shouldBeUnderlined;
-        this.spoiler = shouldBeSpoiler;
+        this.markdown.append(text);
 
         return Optional.empty();
+    }
+
+    private void pushTokens(Style style) {
+        if (!this.lastStyle.isBold() && style.isBold()) {
+            this.markdown.append(BOLD_TOKEN);
+        }
+
+        if (!this.lastStyle.isItalic() && style.isItalic()) {
+            this.markdown.append(ITALIC_TOKEN);
+        }
+
+        if (!this.lastStyle.isUnderlined() && style.isUnderlined()) {
+            this.markdown.append(UNDERLINED_TOKEN);
+        }
+
+        if (!this.lastStyle.isStrikethrough() && style.isStrikethrough()) {
+            this.markdown.append(STRIKETHROUGH_TOKEN);
+        }
+
+        if (!this.lastStyle.isObfuscated() && style.isObfuscated()) {
+            this.markdown.append(SPOILER_TOKEN);
+        }
+
+        if (!this.isInlineCode(this.lastStyle) && this.isInlineCode(style)) {
+            this.markdown.append(INLINE_CODE_TOKEN);
+        }
+    }
+
+    private void popTokens(Style style) {
+        if (this.isInlineCode(this.lastStyle) && !this.isInlineCode(style)) {
+            this.markdown.append(INLINE_CODE_TOKEN);
+        }
+
+        if (this.lastStyle.isObfuscated() && !style.isObfuscated()) {
+            this.markdown.append(SPOILER_TOKEN);
+        }
+
+        if (this.lastStyle.isStrikethrough() && !style.isStrikethrough()) {
+            this.markdown.append(STRIKETHROUGH_TOKEN);
+        }
+
+        if (this.lastStyle.isUnderlined() && !style.isUnderlined()) {
+            this.markdown.append(UNDERLINED_TOKEN);
+        }
+
+        if (this.lastStyle.isItalic() && !style.isItalic()) {
+            this.markdown.append(ITALIC_TOKEN);
+        }
+
+        if (this.lastStyle.isBold() && !style.isBold()) {
+            this.markdown.append(BOLD_TOKEN);
+        }
+
+        this.lastStyle = style;
+    }
+
+    /**
+     * If the component can be interacted with in any way, we will treat it as
+     * pseudocode, representing it as an inline codeblock in markdown
+     *
+     * @param style The component style
+     * @return True if the component is clickable or has a tooltip
+     */
+    private boolean isInlineCode(Style style) {
+        return style.getHoverEvent() != null || style.getClickEvent() != null;
     }
 }
